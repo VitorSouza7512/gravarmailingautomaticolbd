c1beba237caac8146a2150550ac0560983420180
VitorSouza7512 <vitorsouza7512@gmail.com>
2024-10-23 19:49:35 -0300

    Ajuste lambda

---
+++ config/exit.sh
@@ -0,0 +1,5 @@
+
+
+
+## Deleta a fila que foi criada localmente
+ aws --endpoint-url=http://localhost:4566 sqs delete-queue --queue-url http://sqs.us-east-1.localhost.localstack.cloud:4566/000000000000/gravar-mailing-automatico-sqs
---
+++ config/start.sh
@@ -0,0 +1,6 @@
+## Criar fila localmente na aws
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name gravar-mailing-automatico-sqs
+
+## Posta mensagem na fila aws
+aws --endpoint-url=http://localhost:4566 sqs send-message --region us-east-1 --queue-url "http://sqs.us-east-1.localhost.localstack.cloud:4566/000000000000/gravar-mailing-automatico-sqs"  --message-body file://mensagem.json
+
